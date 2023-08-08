200ca88523d63d4d167f2c51097eb7af6ef91944
Tom-Håkon Fjeldheim <tom.fjeldheim@gmail.com>
2023-08-08 13:28:06 +0200

    est

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <main class="dark:bg-[#141D2F] bg-[#F6F8FF] h-screen">
-  <p>Hei</p>
-    <!-- <div class="dark:bg-[#141D2F] lg:px-48 xd:px-62 md:px-20 sm:pt-10 pb-5">
+    <div class="dark:bg-[#141D2F] lg:px-48 xd:px-62 md:px-20 sm:pt-10 pb-5">
       <headerBar class="" />
       <searchBar
         class="dark:bg-[#1E2A47] bg-[#FEFEFE] mx-7 rounded-2xl p-2 mb-5"
       />
       <mainContent />
-    </div> -->
+    </div>
   </main>
 </template>
 <script setup>
-// import headerBar from "./components/headerBar.vue";
-// import searchBar from "./components/searchBar.vue";
-// import mainContent from "./components/mainContent.vue";
+import headerBar from "./components/headerBar.vue";
+import searchBar from "./components/searchBar.vue";
+import mainContent from "./components/mainContent.vue";
 </script>
